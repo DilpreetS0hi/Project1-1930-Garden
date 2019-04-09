c911efb9ba271f3fe11433192ff37248062d279f
royroyzhong <zhongroy189@gmail.com>
2019-04-09 10:57:44 -0700

    optimize code

---
+++ DoneWithJS/largegardenlayoutherb333.js
@@ -0,0 +1,49 @@
+ document.getElementById("button1").addEventListener("click", function () {
+     Showlist("harvest with kitchen window");
+
+
+
+ });
+
+ function Showlist(catergory) {
+     var dbRef = firebase.database().ref("Team15/Combination/" + catergory);
+     var promise = dbRef.once("value", function (snap) {
+         list = snap.val();
+     });
+
+
+     promise.then(function () {
+         DisplayList(list); //JSON object
+     });
+ }
+
+
+
+
+
+
+ var indexofId = 0;
+
+ function DisplayList(list) {
+     console.log(list);
+
+     for (x in list) {
+
+         indexofId++;
+         DisplayImage("./images/" + x + ".png");
+     }
+
+ }
+
+ function DisplayImage(picturefile) {
+     var x = document.createElement("IMG");
+     x.setAttribute("src", picturefile);
+     x.setAttribute("width", "100");
+     x.setAttribute("height", "100");
+     x.setAttribute("id", "listofplant" + indexofId);
+     x.setAttribute("draggable", "true");
+     x.setAttribute("ondragstart", "drag(event)");
+     var pa = document.getElementById("showit");
+     pa.appendChild(x);
+     //                        document.body.appendChild(x);
+ }
